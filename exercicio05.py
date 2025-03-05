86228ba0785ca6d165a81ff9bca77d603746dd45
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 14:11:29 -0300

    exercicio05.py completo

---
+++ exercicio05.py
@@ -0,0 +1,12 @@
+print('')
+
+while True:
+    try:
+        valor_1 = float(input('Digite um número: '))
+        break
+    except ValueError:
+        print('Número inválido.\n')
+
+quadrado = valor_1 * valor_1
+
+print(f'O quadrado de {valor_1} é {quadrado}\n')
