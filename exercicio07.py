e71ce31efa30d4e5e50b3516d50f34a90055d96c
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 15:58:37 -0300

    exercicio07.py completo

---
+++ exercicio07.py
@@ -0,0 +1,18 @@
+print('')
+while True:
+    try:
+        valor_1 = float(input('Digite o primeiro número: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.\n')
+
+while True:
+    try:
+        valor_2 = float(input('Digite o segundo número: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.')
+
+media = (valor_1 + valor_2)/2
+
+print(f'A média dos números fornecidos é {media}.\n')
