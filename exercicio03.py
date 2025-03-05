18c68a0b2c3d6a98338f9a525a46f30d115082b4
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 11:22:26 -0300

    exercicio03.py completo

---
+++ exercicio03.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        print('')
+        valor_1 = float(input('Digite o primeiro número: '))
+        break
+    except:
+        print('Digite um valor númérico válido.')
+
+while True:
+    try:
+        valor_2 = float(input('Digite o segundo número: '))
+        break
+    except:
+        print('Digite um valor númérico válido.')
+
+total = valor_1 * valor_2
+
+print(f'A multiplicação de {valor_1} por {valor_2} é {total}\n')
+
+
