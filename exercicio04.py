faa39cc96a807169835b6d904353408cd42e309e
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 12:15:15 -0300

    exercicio04.py completo

---
+++ exercicio04.py
@@ -0,0 +1,25 @@
+print('')
+
+# Solicita o primeiro número inteiro
+while True:
+    try:
+        valor_1 = int(input('Digite o primeiro número inteiro: '))
+        break
+    except ValueError:
+        print('Número inteiro inválido.\n')
+
+# Solicita o segundo número inteiro e evita divisão por zero
+while True:
+    try:
+        valor_2 = int(input('Digite o segundo número inteiro: '))
+        if valor_2 == 0:  # Verifica se o usuário digitou zero
+            raise ZeroDivisionError  # Lança o erro para cair no except
+        break
+    except ValueError:
+        print('Número inteiro inválido.\n')
+    except ZeroDivisionError:
+        print('Não é possível dividir um número por 0.\n')
+
+# Realiza a divisão apenas se os valores forem válidos
+total = valor_1 / valor_2
+print(f'A divisão de {valor_1} por {valor_2} é {total:.2f}\n')
