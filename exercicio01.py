55fc3944c6646977d56cd144b4a6450c9d218e63
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 10:05:24 -0300

    exercicio01.py completo

---
+++ exercicio01.py
@@ -0,0 +1,17 @@
+while True:  # Loop infinito até receber um número inteiro
+    try:
+        valor_1 = int(input('Digite o primeiro valor inteiro: '))
+        break  # Sai do loop se a conversão for bem-sucedida
+    except ValueError:
+        print('O valor precisa ser um número inteiro. Tente novamente. \n')
+
+while True:  # Loop infinito até receber um número inteiro
+    try:
+        valor_2 = int(input('Digite o segundo valor inteiro: '))
+        break  # Sai do loop se a conversão for bem-sucedida
+    except ValueError:
+        print('O valor precisa ser um número inteiro. Tente novamente. \n')
+
+soma = valor_1 + valor_2
+
+print(f'A soma de {valor_1} + {valor_2} é igual a {soma}\n')
