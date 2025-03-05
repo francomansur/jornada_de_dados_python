522cdbdd8b7cf0dc6fb04b336fa699d6763195f7
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-05 16:11:45 -0300

    exercicio08.py completo

---
+++ exercicio08.py
@@ -0,0 +1,18 @@
+print('')
+while True:
+    try:
+        base = float(input('Digite a base da potencia: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.\n')
+
+while True:
+    try:
+        expoente = float(input('Digite o expoente da potencia: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.\n')
+
+resultado = base ** expoente
+
+print(f'A potencia de {base} ^ {expoente} é {resultado}.\n')
