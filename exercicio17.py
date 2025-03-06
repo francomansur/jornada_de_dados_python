676dd27b36a2135b7728e737d331dbdb42ec5a29
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-06 14:26:18 -0300

    exercicio17.py completo

---
+++ exercicio17.py
@@ -0,0 +1,35 @@
+"""
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+"""
+
+print('')
+
+while True:
+    try:
+        entrada = input('Digite o primeiro valor Booleano (True ou False): ').strip().capitalize() 
+        if entrada == "True":
+            valor_1 = True
+        elif entrada == "False":
+            valor_1 = False
+        else:
+            raise ValueError 
+        break
+    except ValueError:
+        print('Digite um valor Booleano válido (True/False).\n')
+
+while True:
+    try:
+        entrada = input('Digite o segundo valor Booleano (True ou False): ').strip().capitalize()
+        if entrada == "True":
+            valor_2 = True
+        elif entrada == "False":
+            valor_2 = False
+        else:
+            raise ValueError
+        break
+    except ValueError:
+        print('Digite um valor Booleano válido (True/False).\n')
+
+resultado = valor_1 or valor_2
+
+print(f'O OR lógico entre {valor_1} e {valor_2} é {resultado}.\n')
