2acb2321d4bde81a29c0a984915c1638fed9047f
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-06 12:14:39 -0300

    exercicio15.py completo

---
+++ exercicio15.py
@@ -0,0 +1,12 @@
+"""
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+"""
+
+print('')
+
+primeira_frase = str(input('Digite a primeira frase: '))
+segunda_frase = str(input('Digite a segunda frase: '))
+
+concat_frase = primeira_frase + ' ' + segunda_frase
+
+print(f'Frase concatenada: {concat_frase}.\n')
