243c12699f40ce0a07009b7aa98c1055e6f85304
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-06 14:11:55 -0300

    exercicio16.py completo

---
+++ exercicio16.py
@@ -0,0 +1,35 @@
+"""
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+"""
+
+print('')
+
+while True:
+    try:
+        entrada = input('Digite o primeiro valor Booleano (True ou False): ').strip().capitalize() 
+        if entrada == "True":
+            valor_1 = True
+        elif entrada == "False":
+            valor_1 = False
+        else:
+            raise ValueError 
+        break
+    except ValueError:
+        print('Digite um valor Booleano válido (True/False).\n')
+
+while True:
+    try:
+        entrada = input('Digite o segundo valor Booleano (True ou False): ').strip().capitalize()
+        if entrada == "True":
+            valor_2 = True
+        elif entrada == "False":
+            valor_2 = False
+        else:
+            raise ValueError
+        break
+    except ValueError:
+        print('Digite um valor Booleano válido (True/False).\n')
+
+resultado = valor_1 and valor_2
+
+print(f'O AND lógico entre {valor_1} e {valor_2} é {resultado}.\n')
