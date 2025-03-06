4d2c24ec57a691dc6d9f87dfeda0d6381c6d96ba
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-06 11:40:11 -0300

    exercicio13.py completo

---
+++ exercicio13.py
@@ -0,0 +1,9 @@
+"""
+13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+"""
+
+print('')
+
+frase = str(input('Digite uma frase: '))
+
+print(f'Frase formatada sem espaços iniciais/finais: "{frase.strip()}".\n')
