9354438952abf3e46a44ee96a7b8dbecba7a2900
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-06 14:31:27 -0300

    exercicio18.py completo

---
+++ exercicio18.py
@@ -0,0 +1,22 @@
+"""
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+"""
+
+print('')
+
+while True:
+    try:
+        entrada = input('Digite um valor Booleano (True ou False): ').strip().capitalize() 
+        if entrada == "True":
+            valor = True
+        elif entrada == "False":
+            valor = False
+        else:
+            raise ValueError 
+        break
+    except ValueError:
+        print('Digite um valor Booleano válido (True/False).\n')
+
+resultado = not valor
+
+print(f'O resultado do NOT lógico de {valor} é {resultado}.\n')
