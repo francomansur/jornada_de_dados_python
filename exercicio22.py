6da6b4512d2ab69ef8f18cde2b448e1dd6c9b5da
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-07 09:48:10 -0300

    exercicio22.py completo

---
+++ exercicio22.py
@@ -0,0 +1,16 @@
+"""
+    22. Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações).
+"""
+
+print('')
+
+while True:
+    try:
+        palavra = str(input('Digite uma palavra: ')).strip()
+        break
+    except ValueError:
+        print('Você não digitou uma palavra.\n')
+
+palindromo = (palavra == palavra[::-1])
+
+print(f'Palíndromo: {palindromo}\n')
