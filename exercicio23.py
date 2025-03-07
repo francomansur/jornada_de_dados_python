118748705b5fda25087ef87474b4420847c983a0
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-07 10:38:31 -0300

    exercicio23.py completo

---
+++ exercicio23.py
@@ -0,0 +1,46 @@
+"""
+    23. Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário.
+"""
+
+print('')
+
+while True:
+    try:
+        numero_1 = float(input('Digite o primeiro número: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.\n')
+
+while True:
+    try:
+        numero_2 = float(input('Digite o segundo número: '))
+        break
+    except ValueError:
+        print('O valor digitado não é um número.\n')
+
+while True:
+    try:
+        operador = str(input('Digite o operador da conta (+, -, *, /): '))
+        if operador == ('+'):
+            resultado = numero_1 + numero_2
+            print(f'{numero_1} {operador} {numero_2} = {resultado}\n')
+            break
+        elif operador == ('-'):
+            resultado = numero_1 - numero_2
+            print(f'{numero_1} {operador} {numero_2} = {resultado}\n')
+            break
+        elif operador == ('*'):
+            resultado = numero_1 * numero_2
+            print(f'{numero_1} {operador} {numero_2} = {resultado}\n')
+            break
+        elif operador == ('/'):
+            resultado = numero_1 / numero_2
+            print(f'{numero_1} {operador} {numero_2} = {resultado}\n')
+            break
+        else:
+            raise ValueError
+    except ValueError:
+        print('Operador inválido.\n')
+
+
+
