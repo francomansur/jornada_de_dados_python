066a0ad6e117871cea1168d4c2f4722a86dcea20
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-10 11:50:16 -0300

    exercicio03.py completo

---
+++ aula_03/exercicio03.py
@@ -0,0 +1,13 @@
+"""    
+    Exercício 3:
+    Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'. 
+    Dado um registro de log em formato de dicionário como log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, 
+    escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+"""
+
+print('')
+
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+
+if (log['level'] == 'ERROR'):
+    print(f'{log['message']}\n')
