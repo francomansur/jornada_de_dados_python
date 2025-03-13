2a999acc705b3a972e6972be9eb541f7f8db27b0
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-13 10:50:53 -0300

    exercicio06.py completo

---
+++ aula_03/exercicio06.py
@@ -0,0 +1,23 @@
+"""    
+    Exercício 6: Contagem de Palavras em Textos
+    Dado um texto, contar quantas vezes cada palavra única aparece nele.
+"""
+
+print('')
+
+texto = str(input('Digite seu texto: '))
+
+texto_palavras = texto.lower().split()
+texto_count = {}
+
+for palavra in texto_palavras:
+    if palavra in texto_count:
+        texto_count[palavra] += 1
+    else:
+        texto_count[palavra] = 1
+
+print("\nContagem de palavras:")
+for palavra, contagem in texto_count.items():
+    print(f"{palavra}: {contagem}")
+
+print('')
