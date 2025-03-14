377939586accdc20fe41e1bcd8199df5d06d8a56
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-14 08:34:25 -0300

    exercicio09.py completo

---
+++ aula_03/exercicio09.py
@@ -0,0 +1,12 @@
+"""    
+    Exercício 9: Extração de Subconjuntos de Dados
+    Dada uma lista de números, extrair apenas aqueles que são pares.
+"""
+
+print('')
+
+numeros = range(1, 11)
+numeros_pares = [numero for numero in numeros if numero %2 == 0]
+
+print(numeros_pares)
+
