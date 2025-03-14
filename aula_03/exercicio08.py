597670ea9dad735e466e32c2041fbf32870fd4f3
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-14 08:31:07 -0300

    exercicio08.py completo

---
+++ aula_03/exercicio08.py
@@ -0,0 +1,16 @@
+"""    
+    Exercício 8: Filtragem de Dados Faltantes
+    Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando.
+"""
+
+print('')
+
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+usuarios_validos = [usuario for usuario in usuarios if usuario["email"]]
+
+print(usuarios_validos)
