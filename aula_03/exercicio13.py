358c6a83de081109f6a40b97bb54d1e49ee342fe
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-19 10:20:56 -0300

    exercicio13.py completo

---
+++ aula_03/exercicio13.py
@@ -0,0 +1,15 @@
+"""    
+    Exercício 13: Consumo de API Simulado
+    Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+"""
+
+print('')
+
+pagina_atual = 1
+pagina_total = 5
+
+while pagina_atual <= pagina_total:
+    print(f'Pagina {pagina_atual}/{pagina_total} processada.')
+    pagina_atual += 1
+
+print('\nTodas as páginas foram processadas.\n')
