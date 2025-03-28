5e41420fd5c21ae6745c0aff50cf3f8b362724d5
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-28 08:31:53 -0300

    base_1.py corrigido, agora mostra as posicoes

--- aula_04/fundamentos/base_1.py
+++ aula_04/fundamentos/base_1.py
@@ -8,4 +8,12 @@ for x in range(5):
         except ValueError:
             print('Número inválido.\n')
 
-print(f'O maior valor digitado é {max(numeros)}, e o menor é {min(numeros)}.')
+max = (max(numeros))
+min = (min(numeros))
+
+posicoes_max = [str(i) for i, n in enumerate(numeros) if n == max]
+posicoes_min = [str(i) for i, n in enumerate(numeros) if n == min]
+
+print(f'\nO maior valor digitado é {max}, nas posições: {"...".join(posicoes_max)}')
+print(f'O maior valor digitado é {min}, nas posições: {"...".join(posicoes_min)}\n')
+
