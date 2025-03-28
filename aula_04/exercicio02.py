31837c6f29541020a8c77db7530afe025cf2c2e9
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-28 10:23:31 -0300

    exercicio02.py aula 4 completo

---
+++ aula_04/exercicio02.py
@@ -0,0 +1,14 @@
+"""    
+    Exercício 2: Modificar lista de linguagens
+    Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+"""
+
+print('')
+
+lista = ["Python", "Java", "C++", "JavaScript"]
+print(f'\nLista antes: {lista}')
+
+lista.remove('C++')
+lista.append('Ruby')
+
+print(f'Lista depois: {lista}\n')
