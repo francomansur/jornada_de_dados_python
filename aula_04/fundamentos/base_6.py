84d15dcae81a89f4e6cc54986c05ee35fcd787b0
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-03-28 08:37:54 -0300

    base_6.py atualizado

---
+++ aula_04/fundamentos/base_6.py
@@ -0,0 +1,26 @@
+abre = []
+fecha = []
+
+while True:
+    try:
+        frase_1 = input('\nDigite uma expressão com parênteses (): ')
+        
+        if "("  in frase_1 or ")" in frase_1:
+            break
+        else:
+            raise ValueError
+
+    except ValueError:
+        print('A expressão deve conter parênteses.')
+
+for x in frase_1:
+    if x == '(':
+        abre.append(x)
+    elif x == ')':
+        fecha.append(x)
+
+if (len(abre)) != (len(fecha)):
+    print('\nExpressão inválida.\n')
+else:
+    print('\nExpressão válida.\n')
+    
