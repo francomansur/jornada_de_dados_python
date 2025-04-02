5c8f590e2fa901c6c029abb34eaf0e7636925b5a
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-04-02 15:10:06 -0300

    exercicio09.py aula 4 completo

---
+++ aula_04/exercicio09.py
@@ -0,0 +1,11 @@
+"""    
+    Exercício 9: Agregação de Dados
+    Dado um conjunto de números, calcular a média.
+"""
+
+print('')
+
+numeros = [10, 20, 30, 40, 50]
+media = sum(numeros) / len(numeros)
+
+print(f'Média: {media}\n')
